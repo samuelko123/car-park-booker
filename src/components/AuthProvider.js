7d742d0ae13a5a77a4ac89eb50d173e9e4a0fae0
Samuel Ko <koshunyin@gmail.com>
2022-07-16 00:26:58 +1000

    try useSWR on AuthProvider

--- src/components/AuthProvider.js
+++ src/components/AuthProvider.js
@@ -1,42 +1,24 @@
-import axios from 'axios'
 import React from 'react'
-import { HTTP_METHOD } from '../utils/constants'
+import axios from 'axios'
+import useSWR from 'swr'
 
 export const AuthContext = React.createContext({})
 
 export const AuthProvider = (props) => {
 	const { children } = props
 
-	const [user, setUser] = React.useState(null)
-	const [error, setError] = React.useState(null)
-	const [loading, setLoading] = React.useState(true)
-
-	React.useEffect(() => {
-		async function sendRequest() {
-			try {
-				setError(null)
-				setLoading(true)
-				const request = {
-					url: '/api/session',
-					method: HTTP_METHOD.GET,
-				}
-				const res = await axios.request(request)
-				setUser(res.data)
-			} catch (err) {
-				setError(err)
-			} finally {
-				setLoading(false)
-			}
-		}
-		sendRequest()
-	}, [])
+	const fetcher = url => axios.get(url).then(res => res.data)
+	const {
+		data: user,
+		error,
+	} = useSWR('/api/session', fetcher)
 
 	return (
 		<AuthContext.Provider
 			value={{
 				user: user,
 				error: error,
-				loading: loading,
+				loading: user === undefined,
 			}}
 		>
 			{children}
